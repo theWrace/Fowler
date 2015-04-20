283f5721ffa8b7655b727265a2ad86791f557e80
theWrace <marcel.pisot@gmx.de>
2015-04-20 10:44:38 +0200

    Moved method for frequentRenterPoints to Rental

--- src/Rental.java
+++ src/Rental.java
@@ -34,4 +34,12 @@ class Rental {
 		}
 		return result;
 	}
+
+	public int frequentRenterPoints() {
+		// add frequent renter points
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
+            return 2;
+        return 1;
+	}
 }
